990d48e7dce02e03bbddfe2b947be3b3f5adadef
srafi1 <shakilrafi0@gmail.com>
2017-10-15 14:13:48 -0400

    done

--- db_builder.py
+++ db_builder.py
@@ -3,24 +3,33 @@
 # HW09 -- No Treble
 # 2017-10-15
 
-import sqlite3   #enable control of an sqlite database
-import csv       #facilitates CSV I/O
+import sqlite3   # enable control of an sqlite database
+import csv       # facilitates CSV I/O
 
+f='discobandit.db'
 
-f="discobandit.db"
+db = sqlite3.connect(f) # open if f exists, otherwise create
+c = db.cursor()    # facilitate db ops
 
-db = sqlite3.connect(f) #open if f exists, otherwise create
-c = db.cursor()    #facilitate db ops
+command = 'CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER)'
+c.execute(command) # create table
+courses = open('courses.csv', 'r')
+reader = csv.DictReader(courses)
+for row in reader:
+    command = 'INSERT INTO courses VALUES ("%s", %s, %s)' % (row['code'], row['mark'], row['id'])
+    # print command
+    c.execute(command) # add given row to table
 
-#==========================================================
-#INSERT YOUR POPULATE CODE IN THIS ZONE
+command = 'CREATE TABLE peeps(name TEXT, age INTEGER, id INTEGER PRIMARY KEY)'
+c.execute(command)
+peeps = open('peeps.csv', 'r')
+reader = csv.DictReader(peeps)
+for row in reader:
+    command = 'INSERT INTO peeps VALUES ("%s", %s, %s)' % (row['name'], row['age'], row['id'])
+    # print command
+    c.execute(command)
 
-
-command = ""          #put SQL statement in this string
-c.execute(command)    #run SQL statement
-
-#==========================================================
-db.commit() #save changes
-db.close()  #close database
+db.commit() # save changes
+db.close()  # close database
 
 
