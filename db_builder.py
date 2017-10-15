97e09ac1870503cd653d0162e9b50c1dc4170808
srafi1 <shakilrafi0@gmail.com>
2017-10-15 13:02:40 -0400

    init

---
+++ db_builder.py
@@ -0,0 +1,26 @@
+# Shakil Rafi
+# SoftDev1 pd7
+# HW09 -- No Treble
+# 2017-10-15
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitates CSV I/O
+
+
+f="discobandit.db"
+
+db = sqlite3.connect(f) #open if f exists, otherwise create
+c = db.cursor()    #facilitate db ops
+
+#==========================================================
+#INSERT YOUR POPULATE CODE IN THIS ZONE
+
+
+command = ""          #put SQL statement in this string
+c.execute(command)    #run SQL statement
+
+#==========================================================
+db.commit() #save changes
+db.close()  #close database
+
+
